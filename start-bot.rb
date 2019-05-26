22e216f47b427744110f9dae8af12523d793156a
Nishant Arora <nish54@gmail.com>
2019-05-26 01:10:10 -0400

    stock working

--- start-bot.rb
+++ start-bot.rb
@@ -42,16 +42,35 @@ bot.command(:weather) do |event|
     weather_data = JSON.parse(response)
 
     to_print = []
-    to_print << "Location: ........... " + weather_data["name"].to_s.downcase
-    to_print << "Temperature: ... " + (((weather_data["main"]["temp"].to_f - 273) * 9 / 5) + 32).ceil(2).to_s + ' °F'
-    to_print << "Weather: ........... " + weather_data["weather"][0]["main"].to_s.downcase
-    to_print << "Description: ...... " + weather_data["weather"][0]["description"].to_s
-    to_print << "Humidity: .......... " + weather_data["main"]["humidity"].to_s + '%'
-    to_print << "Low: .................... " + (((weather_data["main"]["temp_min"].to_f - 273) * 9 / 5) + 32).ceil(2).to_s + ' °F'
-    to_print << "High: ................... " + (((weather_data["main"]["temp_max"].to_f - 273) * 9 / 5) + 32).ceil(2).to_s + ' °F'
-    to_print << "Wind: ................. " + weather_data["wind"]["speed"].to_s + " mph"
-    to_print << "Sunrise: .............. " + Time.at((weather_data["sys"]["sunrise"].to_f)).to_s.split(' ')[1].to_s
-    to_print << "Sunset: ............... " + Time.at((weather_data["sys"]["sunset"].to_f)).to_s.split(' ')[1].to_s
+    to_print << "Location: ............. " + weather_data["name"].to_s.downcase
+    to_print << "Temperature: ..... " + (((weather_data["main"]["temp"].to_f - 273) * 9 / 5) + 32).ceil(2).to_s + ' °F'
+    to_print << "Weather: ............. " + weather_data["weather"][0]["main"].to_s.downcase
+    to_print << "Description: ........ " + weather_data["weather"][0]["description"].to_s
+    to_print << "Humidity: ............ " + weather_data["main"]["humidity"].to_s + '%'
+    to_print << "Low: ...................... " + (((weather_data["main"]["temp_min"].to_f - 273) * 9 / 5) + 32).ceil(2).to_s + ' °F'
+    to_print << "High: ..................... " + (((weather_data["main"]["temp_max"].to_f - 273) * 9 / 5) + 32).ceil(2).to_s + ' °F'
+    to_print << "Wind: ................... " + weather_data["wind"]["speed"].to_s + " mph"
+    to_print << "Sunrise: ................ " + Time.at((weather_data["sys"]["sunrise"].to_f)).strftime("%l:%M %p").to_s.lstrip()
+    to_print << "Sunset: ................. " + Time.at((weather_data["sys"]["sunset"].to_f)).strftime("%l:%M %p").to_s.lstrip()
+
+    event.respond to_print.join("\n")
+end
+
+bot.command(:stock) do |event|
+    api_key = '3KT67VI8256BBOXC'
+    symbol = event.message.content.split(' ')[1].upcase
+    request = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=#{symbol}&apikey=#{api_key}"
+    response = open(request).readlines.join
+    stock_data = JSON.parse(response)
+
+    to_print = []
+    most_recent = stock_data["Meta Data"]["3. Last Refreshed"]
+    info = stock_data["Time Series (Daily)"][most_recent]
+    
+    to_print << "Open ........ " + '%.2f' % info["1. open"].to_f.ceil(2)
+    to_print << "Close ........ " + '%.2f' % info["4. close"].to_f.ceil(2).to_s
+    to_print << "High .......... " + '%.2f' % info["2. high"].to_f.ceil(2).to_s
+    to_print << "Low ........... " + '%.2f' % info["3. low"].to_f.ceil(2).to_s
 
     event.respond to_print.join("\n")
 end
