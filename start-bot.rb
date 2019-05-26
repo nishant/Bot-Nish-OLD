f4a6cb877e7106521ddb15b161f9d8fbf0090e54
Nishant Arora <nish54@gmail.com>
2019-05-26 01:37:47 -0400

    stock search working

--- start-bot.rb
+++ start-bot.rb
@@ -58,19 +58,27 @@ end
 
 bot.command(:stock) do |event|
     api_key = '3KT67VI8256BBOXC'
-    symbol = event.message.content.split(' ')[1].upcase
+    keyword = event.message.content.sub("%stock", "")
+    request = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=#{keyword}&apikey=#{api_key}"
+    response = open(request).readlines.join
+    search_data = JSON.parse(response)
+    to_print = []
+
+    to_print << "Company ...... " + search_data["bestMatches"][0]["2. name"]
+    to_print << "Symbol .......... " + search_data["bestMatches"][0]["1. symbol"]
+
+    symbol = search_data["bestMatches"][0]["1. symbol"]
     request = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=#{symbol}&apikey=#{api_key}"
     response = open(request).readlines.join
     stock_data = JSON.parse(response)
 
-    to_print = []
     most_recent = stock_data["Meta Data"]["3. Last Refreshed"]
     info = stock_data["Time Series (Daily)"][most_recent]
     
-    to_print << "Open ........ " + '%.2f' % info["1. open"].to_f.ceil(2)
-    to_print << "Close ........ " + '%.2f' % info["4. close"].to_f.ceil(2).to_s
-    to_print << "High .......... " + '%.2f' % info["2. high"].to_f.ceil(2).to_s
-    to_print << "Low ........... " + '%.2f' % info["3. low"].to_f.ceil(2).to_s
+    to_print << "Open .............. " + '%.2f' % info["1. open"].to_f.ceil(2)
+    to_print << "Close .............. " + '%.2f' % info["4. close"].to_f.ceil(2).to_s
+    to_print << "High ................ " + '%.2f' % info["2. high"].to_f.ceil(2).to_s
+    to_print << "Low ................. " + '%.2f' % info["3. low"].to_f.ceil(2).to_s
 
     event.respond to_print.join("\n")
 end
